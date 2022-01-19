fd25b86ee4b33710788b9f90c3fc11fb2295bc4a
Nayal <menayal1999@gmail.com>
2022-01-18 19:16:06 -0600

    Created .gitignore. Ignored venv. Created requirements.txt for a list of dependencies. Created openApplications.py for first script on opening webpages and or materials.

---
+++ openApplications.py
@@ -0,0 +1,7 @@
+'''
+This program is going to open up certain websites and relevant materials at certain times.
+I am going to use to to automate opening up my school assignments and materials.
+'''
+def main():
+    if __name__ == "__main__":
+        print("Hello")
